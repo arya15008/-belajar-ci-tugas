472eaae65d8f8a2621550fbab1f9cf9fd1432927
arya 15008 <aryaprasetyawan880@gmail.com>
2025-06-12 22:10:31 +0700

    Upload project awal

---
+++ app/Views/v_faq.php
@@ -0,0 +1,22 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+
+<div class="pagetitle">
+    <h1>Faq</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">Home</li>
+            <li class="breadcrumb-item active">F.A.Q</li>
+        </ol>
+    </nav>
+</div>
+
+<section class="section faq">
+    <div class="card">
+        <div class="card-body pt-3">
+            <p>Ini halaman F.A.Q</p>
+        </div>
+    </div>
+</section>
+
+<?= $this->endSection() ?>
